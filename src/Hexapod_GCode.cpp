2a68a32a9f89721b21a6919e6f380d864f4b3d83
NicHub <nicolas.jeanmonod@gmail.com>
2019-04-21 19:11:38 +0200

    ajout du .cpp et du .h pour le G-Code

---
+++ src/Hexapod_GCode.cpp
@@ -0,0 +1,8 @@
+#include <Hexapod_GCode.h>
+
+/**
+ *
+ */
+Hexapod_GCode::Hexapod_GCode()
+{
+}
---
+++ src/Hexapod_GCode.h
@@ -0,0 +1,12 @@
+#ifndef __HEXAPOD_GCODE_H__
+#define __HEXAPOD_GCODE_H__
+
+#include <main.h>
+
+class Hexapod_GCode
+{
+  public:
+    Hexapod_GCode();
+};
+
+#endif
