00b313dcd9473a32433a46f7b411fcacad809733
NicHub <nicolas.jeanmonod@gmail.com>
2019-05-01 18:10:31 +0200

    md2pdf.sh

---
+++ doc/md2pdf.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+pandoc                               \
+    --from markdown \
+    --to html                        \
+    --standalone                     \
+    --mathml                         \
+    hexapod-kinematics.md            \
+    -o hexapod-kinematics.html
+
+prince --javascript hexapod-kinematics.html
