b7c3a05c14eb1bdcc9bfe033258409e8ad2e6d55
NicHub <nicolas.jeanmonod@gmail.com>
2019-07-07 15:41:52 +0200

    Jupyter

--- doc/sympy/hexapod_kinematics.py
+++ doc/sympy/hexapod_kinematics.py
@@ -1,11 +1,12 @@
-from sympy import *
-from sympy import symbols
-from sympy.interactive.printing import init_printing
-init_printing(use_unicode=False, wrap_line=False)
-from sympy.matrices import Matrix, eye, zeros, ones, diag, GramSchmidt
-from sympy import collect, sympify, Wild
-from sympy.printing import print_ccode
+#!/usr/bin/env python
+# coding: utf-8
 
+# FORWARD KINEMATICS SOLUTION OF A STEWART PLATFORM ACTUATED BY ROTARY MOTORS (DRAFT)
+
+# %%
+
+
+from sympy import *
 
 x, y, A, B, C, Tx, Ty, Ty, Tz, Px, Py, Bx, By, Zhome = symbols("x y A B C Tx Ty Ty Tz Px Py Bx By Zhome")
 
@@ -38,34 +39,35 @@ TB =   Matrix([[      1,       0,       0,       0],
 
 BP = P * Rx * Ry * Rz * Txyz * TB
 
-print("BP = ")
-for elem in BP:
-    print(elem)
-print("")
+
+# %%
+
+print("\n\n\n######### BP #########\n")
+labels = ["BP_x", "BP_y", "BP_z"]
+for cpt, elem in enumerate(BP[:-1]):
+    ans_c = ccode(elem, standard="C99")
+    sep = "\n" if cpt > 0 else ""
+    print(f"{sep}{labels[cpt]} = {ans_c};")
 
 
-init_printing()
+# %%
+
+
 x, y, z, a, b, c, phi, ROD_LENGTH, ARM_LENGTH, theta_s = symbols("x y z a b c phi ROD_LENGTH ARM_LENGTH theta_s")
 
-# Eq du cercle paramétrique
-sol = solve(((ARM_LENGTH*cos(phi)-a)**2 + b**2 + (ARM_LENGTH*sin(phi)-c)**2 - ROD_LENGTH**2), [phi],
-     force=True, manual=True, set=True)
+init_printing()
 
-cpt = 0
-for elem in sol[1]:
-    print(f"\n\nsolution {cpt}")
-    cpt += 1
-    print_ccode(simplify(elem[0]), standard='C99')
+phi_ans = solve(((ARM_LENGTH*cos(phi)-a)**2 + b**2 + (ARM_LENGTH*sin(phi)-c)**2 - ROD_LENGTH**2), [phi],
+          force=True, manual=True)
 
 
-# Eq du cercle carthésienne
-sol = solve(((sqrt(ARM_LENGTH**2-z**2)-a)**2 + b**2 + (sqrt(ARM_LENGTH**2-x**2)-c)**2 - ROD_LENGTH**2), [x, y],
-     force=True, manual=True, set=True)
 
-cpt = 0
-print(sol)
-print(f"{sol[0]}\n")
-for elem in sol[1]:
-    print(f"solution {cpt}\n\n")
-    cpt += 1
-    print_ccode(simplify(elem[0]), standard='C99')
+# %%
+
+
+print("\n\n\n######### PHI #########\n")
+for cpt, elem in enumerate(phi_ans):
+    ans_c = ccode(elem, standard="C99")
+    sep = "\n" if cpt > 0 else ""
+    print(f"{sep}phi_{cpt} = {ans_c};")
+
